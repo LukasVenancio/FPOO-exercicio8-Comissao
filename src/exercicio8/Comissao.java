83cc5d192e021f8aadbcedc83ef46e7353f628ba
Lukas Venancio <lukassvenancio022@gmail.com>
2021-08-27 14:30:25 -0300

    modificações no algorítimo

--- src/exercicio8/Comissao.java
+++ src/exercicio8/Comissao.java
@@ -5,38 +5,35 @@ import java.util.Scanner;
 public class Comissao {
 
 	public static void main(String[] args) {
-		
+
 		Scanner leitor = new Scanner(System.in);
-		
-		double vendas , tempo , comissao;
+
+		double vendas, tempo, comissao;
 		String nome;
-		
+
 		System.out.print("Qual é o nome do vendedor? ");
 		nome = leitor.next();
 		System.out.print("A quantos anos " + nome + " trabalha na empresa? ");
 		tempo = leitor.nextDouble();
 		System.out.print("Qual foi o seu total de vendas de " + nome + " ? ");
 		vendas = leitor.nextDouble();
-		
+
 		leitor.close();
-		
+
 		if (vendas >= 10000 && tempo >= 1) {
 			comissao = (vendas * 5) / 100;
 
-		}else if ( vendas >=20000 && tempo >+ 1 ) {
+		} else if (vendas >= 20000 && tempo >= 1) {
 			comissao = (vendas * 10) / 100;
-			
-		}else {
+
+		} else {
 			comissao = (vendas * 3) / 100;
-			
+
 		}
-		
+
 		System.out.println("");
 		System.out.println("*****************************************************");
 		System.out.println("O valor da comissão de " + nome + " é: R$" + comissao);
-		
-		
-		
 
 	}
 
